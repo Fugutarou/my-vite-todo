19cce8a077c6646761336ba8fb7703088e4067ee
fugu <a@b.com>
2022-10-10 15:06:09 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+import MainTodo from './components/MainTodo.vue'
+</script>
+
+<template>
+  <div class="wrap">
+    <TheHeader />
+    <main class="main">
+      <MainTodo />
+    </main>
+    <TheFooter />
+  </div>
+
+</template>
+
+<style>
+  @import 'assets/css/ress.min.css';
+  .wrap{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100vh;
+    width: 370px;
+    margin: 0 auto;
+    font-family: sans-serif;
+  }
+  .main{
+    flex: 1;
+    width: 100%;
+  }
+</style>
---
+++ src/composables/useTodoList.js
@@ -0,0 +1,61 @@
+import { computed, ref } from 'vue';
+
+export const useTodoList = () => {
+    const ls = localStorage.todoList;
+    const todoListRef = ref([]);
+    todoListRef.value = ls ? JSON.parse(ls) : [];
+    const editId = ref(-1);
+
+    const findById = (id) => {
+        return todoListRef.value.find((todo) => todo.id === id);
+    }
+    const findeIndexById = (id) => {
+        return todoListRef.value.findIndex((todo) => todo.id === id);
+    }
+
+    const add = (task) => {
+        const id = new Date().getTime();
+        todoListRef.value.push({ id: id, task: task, checked: false});
+        localStorage.todoList = JSON.stringify(todoListRef.value);
+    }
+
+    const show = (id) => {
+        const todo = findById(id);
+        editId.value = id;
+        return todo.task; 
+    }
+
+    const edit = (task) => {
+        const todo = findById(editId.value);
+        const idx = findeIndexById(editId.value);
+        todo.task = task;
+        todoListRef.value.splice(idx, 1, todo);
+        localStorage.todoList = JSON.stringify(todoListRef.value);
+        editId.value = -1;
+    }
+
+    const del = (id) => {
+        const todo = findById(id);
+        const delMsg = `「${ todo.task }」を削除しますか？`
+        if(!confirm(delMsg)) return;
+
+        const idx = findeIndexById(id);
+        todoListRef.value.splice(idx, 1);
+        localStorage.todoList = JSON.stringify(todoListRef.value);
+    }
+
+    const check = (id) => {
+        const todo = findById(id);
+        const idx = findeIndexById(id);
+        todo.checked = !todo.checked;
+        todoListRef.value.splice(idx, 1, todo);
+        localStorage.todoList = JSON.stringify(todoListRef.value);
+    }
+
+    const countFin = computed(() => {
+        const finArr = todoListRef.value.filter((todo) => todo.checked);
+        return finArr.length
+    })
+
+    return{ todoListRef, add, show, edit, del, check, countFin };
+}
